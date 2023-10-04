cd6b10f03ca9e0f180d0c691a7245c3112d9bb17
Salman-Irfan <salmanirfan674@gmail.com>
2023-10-04 15:38:00 +0500

    controllers logic for admin side products crud, & data seeded into db using factory

---
+++ app/Http/Resources/ProductListResource.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Facades\URL;
+
+class ProductListResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array<string, mixed>
+     */
+    public function toArray($request)
+    {
+        return [
+            'id' => $this->id,
+            'title' => $this->title,
+            'image_url' => $this->image ?: null,
+            'price' => $this->price,
+            'updated_at' => (new \DateTime($this->updated_at))->format('Y-m-d H:i:s'),
+        ];
+    }
+}
